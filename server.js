ae4ba056ddd44531e34e29e6280a440ec9f6ae9d
saheervc <30866021+saheervc@users.noreply.github.com>
2017-08-14 22:51:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/article-one', function (req, res) {
   res.send('Article One requested and will be served here');
 });
 
-app.get('/article-two', function (req, res) {
-  res.send('Article two requested and will be served here');
+app.get('/article-one', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-three', function (req, res) {
