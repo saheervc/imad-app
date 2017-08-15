59a400b7ac0fb8d4dff85f31ca1dafdff39bde68
saheervc <30866021+saheervc@users.noreply.github.com>
2017-08-15 09:58:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var content = {
+var articleOne = {
     title : 'Article One | Saheer',
     heading: 'Article One',
     date : 'Sep 5 ,2017',
@@ -64,7 +64,7 @@ app.get('/', function (req, res) {
 
 
 app.get('/article-one', function (req, res) {
-   res.send(createTemplate(article-one));
+   res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
