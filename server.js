e64389183611ddc5a778a6bf1a0973076cd9bee5
saheervc <30866021+saheervc@users.noreply.github.com>
2017-08-15 10:42:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.get('/', function (req, res) {
 
 
 app.get('/:articleName', function (req, res) {
-    var articleName=req.param.articleName; 
+    var articleName=req.params.articleName; 
     res.sendFile(createTemplate(articles[articleName]));
 });
 
