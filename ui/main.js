258b260a11af0bfa5202dfa2b8a00fdb8a22be61
saheervc <30866021+saheervc@users.noreply.github.com>
2017-08-21 22:45:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,9 +13,12 @@
 //    var interval = setInterval(moveRight,50);
 //}
 var button=document.getElementById('counter');
+var counter = 0;
 button.onclick = function(){
   
   // Render the variable in the correct span
   counter = counter +1;
+  var span = document.getElementById('count');
+  span.innerHTML=counter.toString();
   
 };
