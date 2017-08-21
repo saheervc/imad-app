26de72746f694b76e25687ae45978e9c8b2e63b8
saheervc <30866021+saheervc@users.noreply.github.com>
2017-08-21 22:40:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,12 +3,12 @@ console.log('Loaded!');
 //var element =document.getElementById('main-text');
 //element.innerHTML='New Text';
 
-var img=document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 4;
-    img.style.marginLeft=marginLeft+ 'px';
-}
-img.onclick=function(){
-    var interval = setInterval(moveRight,50);
-}
+//var img=document.getElementById('madi');
+//var marginLeft = 0;
+//function moveRight(){
+//    marginLeft = marginLeft + 4;
+//    img.style.marginLeft=marginLeft+ 'px';
+//}
+//img.onclick=function(){
+//    var interval = setInterval(moveRight,50);
+//}
